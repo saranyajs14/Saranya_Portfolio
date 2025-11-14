e4532316dea768ceec07c9d44befa002ebda0fbc
Saranya J S <saranyajothisubramanian06@gmail.com>
2025-11-13 23:27:35 -0500

    Update skills-section.tsx

--- components/skills-section.tsx
+++ components/skills-section.tsx
@@ -5,11 +5,12 @@ export default function SkillsSection() {
     programmingLanguages: ["Java", "C++", "Python", "C#", "SQL", "NodeJs"],
     frameworksTools: ["Spring", "Spring Boot", "JUnit", "REST API", "JUnit", "JMeter", "Maven"],
     database: ["DBMS", "Mysql", "MongoDB", "Redis"],
-    Cloud & DevOps: ["AWS", "Terraform", "Docker", "Kubernetes", "Jenkins", "Ansible", "GitHub Actions", "SonarQube", "CI/CD"],
-    Monitoring: ["Grafana", "Splunk", "New Relic", "Sumo Logic"],
-    Architecture: ["Microservices", "Event-Driven Architecture (Kafka)", "Distributed Systems", "Object Oriented Programming"],             
+    CloudDevOps: ["AWS", "Terraform", "Docker", "Kubernetes", "Jenkins", "Ansible", "GitHub Actions", "SonarQube"],
+    monitoring: ["Grafana", "Splunk", "New Relic", "Sumo Logic"],
+    architecture: ["Microservices", "Event-Driven Architecture (Kafka)", "Distributed Systems", "Object Oriented Programming"],
     projectManagement: ["Agile", "Jira", "Kanban", "Confluence"]
-  }
+};
+
 
   const titleRef = useScrollAnimation()
   const categoryRefs = [
